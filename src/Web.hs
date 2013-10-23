fd464fcec9334c1241e0120f6f8ea91c918c6c37
Joel Lehtonen <joel.lehtonen@iki.fi>
2013-10-23 21:53:07 +0300

    Trying to emulate states with a stateless receiver

--- src/Web.hs
+++ src/Web.hs
@@ -4,6 +4,7 @@ module Main where
 import Control.Monad.IO.Class
 import Control.Monad (replicateM_)
 import Data.ByteString.Lazy.Char8 (ByteString)
+import Data.IORef -- Not good for threads but less dependecies than STM
 import Data.Text (Text,unpack)
 import Data.Text.Read (decimal)
 import Network.HTTP.Types (ok200,badRequest400)
@@ -17,24 +18,37 @@ main = do
   let port = 3000
   putStrLn $ "Connecting " ++ show device ++ " and listening to port " ++ show port
   h <- new device
-  run port $ app h
+  var <- newIORef False
+  run port $ app var h
 
-app :: FtdiHandle -> Application
-app h req = case (requestMethod req,pathInfo req) of
-  ("POST",["onoff"]) -> do
+app :: IORef Bool -> FtdiHandle -> Application
+app var h req = case (requestMethod req,pathInfo req) of
+  ("POST",["reset"]) -> do
+    -- Reset state back to false if gets into desync
     liftIO $ onoff h
+    liftIO $ writeIORef var False
     good "OK\n"
-  ("POST",["onoff",skips]) -> do
+  ("POST",["on",skips]) -> do
+    liftIO $ unlessVar False True $ onoff h >> putStrLn "ajoalas"
     case validateSkips skips of
       Just s -> do
         liftIO $ onoff h
         liftIO $ replicateM_ s $ function h
         good "OK\n"
       Nothing -> bad "Mode must be a integer between 0 and 37, inclusive\n"
+  ("POST",["off"]) -> do
+    liftIO $ unlessVar False False $ (putStrLn "ajetaan alas" >> onoff h)
+    good "OK\n"
   ("POST",["function"]) -> do
     liftIO $ function h
     good "OK\n"
-  _ -> bad "Unknown command"
+  _ -> bad "Unknown command\n"
+  where
+    unlessVar test newValue act = do
+      x <- atomicModifyIORef var (\a -> (newValue,a==test))
+      if x
+        then return ()
+        else act
 
 bad,good :: Monad m => ByteString -> m Response
 bad  = textualResponse badRequest400 
