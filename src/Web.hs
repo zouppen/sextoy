42c3b817bbffbf0c9c6ce48fa4a3fd7884d7d7a7
Joel Lehtonen <joel.lehtonen@koodilehto.fi>
2014-03-29 21:37:24 +0200

    Nice partycoded CSS style by Yaamboo

--- src/Web.hs
+++ src/Web.hs
@@ -44,6 +44,9 @@ app var h req = case (requestMethod req,pathInfo req) of
   ("GET",["main.js"]) -> do
     fn <- liftIO $ getDataFileName "main.js"
     return $ ResponseFile ok200 [("Content-Type", "application/javascript")] fn Nothing
+  ("GET",["main.css"]) -> do
+    fn <- liftIO $ getDataFileName "main.css"
+    return $ ResponseFile ok200 [("Content-Type", "text/css")] fn Nothing
   ("POST",["reset"]) -> do
     -- Reset state back to false if gets into desync
     liftIO $ onoff h
